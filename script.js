36d1e65178e70a90c87f6eb270a2ea9d35c9ed8b
RemmyKyalo <chaloremmy@gmail.com>
2023-10-11 14:00:18 +0300

    fetch images from unsplash api

---
+++ script.js
@@ -0,0 +1,32 @@
+const accessKey = "NClEd1riL1XXgZeumwv60X-q0EnNg_cyY1ViqfVAA4A";
+const bodyContainer = document.querySelector(".body-container");
+const searchInput = document.querySelector(".input-bar");
+const searchBtn = document.querySelector(".search-btn");
+
+async function fetchImages(page) {
+  const query = searchInput.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  data.results.map((item) => {
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("image-container");
+    bodyContainer.appendChild(imageContainer);
+
+    const image = document.createElement("img");
+
+    imageContainer.appendChild(image);
+
+    image.src = item.urls.small;
+  });
+}
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let page = 1;
+
+  fetchImages(page);
+
+  searchInput.innerHTML = "";
+});
